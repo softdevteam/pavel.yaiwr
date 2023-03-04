ac43b4983586f15cbf83d7b7e5c3d4a2230ecf22
Pavel Durov <p3ld3v@pm.me>
2023-03-04 12:42:25 +0000

    support for one-time execution alongside repl

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,39 @@
-use std::io::{self, stdout, BufRead, Write};
+use std::{
+    env,
+    io::{self, stdout, BufRead, Write},
+};
 use yaiwr::Calc;
 
 fn main() {
-    let stdin = io::stdin();
-    loop {
-        print!("👉 ");
-        stdout().flush().ok();
-        match stdin.lock().lines().next() {
-            Some(Ok(ref l)) => {
-                if l.trim().is_empty() {
-                    continue;
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        eval_print(&args[1]);
+    } else {
+        let stdin = io::stdin();
+        loop {
+            print!("👉 ");
+            stdout().flush().ok();
+            match stdin.lock().lines().next() {
+                Some(Ok(ref l)) => {
+                    if l.trim().is_empty() {
+                        continue;
+                    }
+                    eval_print(l);
                 }
-                let ast = Calc::from_str(l);
-                match ast {
-                    Ok(opcode) => match Calc::eval(opcode) {
-                        Ok(i) => println!("Result: {}", i),
-                        Err(msg) => eprintln!("Evaluation error: {}", msg),
-                    },
-                    Err(msg) => eprintln!("Evaluation error: {}", msg),
-                }
+                _ => break,
             }
-            _ => break,
         }
+    }
+}
+
+fn eval_print(input: &str) {
+    let ast = Calc::from_str(input);
+    match ast {
+        Ok(opcode) => match Calc::eval(opcode) {
+            Ok(i) => println!("Result: {}", i),
+            Err(msg) => eprintln!("Evaluation error: {}", msg),
+        },
+        Err(msg) => eprintln!("Evaluation error: {}", msg),
     }
 }
