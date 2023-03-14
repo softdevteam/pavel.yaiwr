c8204f5d4adb3bc12de8fec990a2140464c15b05
Pavel Durov <p3ld3v@pm.me>
2023-03-14 17:42:46 +0000

    semicolumn separated statements

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,9 @@ lrlex_mod!("calc.l");
 lrpar_mod!("calc.y");
 
 pub mod ast;
-pub mod err;
 pub mod instruction;
 
 use ast::AstNode;
-use err::InterpError;
 
 pub struct Calc {
     var_store: HashMap<String, u64>,
@@ -26,37 +24,37 @@ impl Calc {
         }
     }
 
-    pub fn get_var(&self, id: String) -> Result<&u64, InterpError> {
+    pub fn get_var(&self, id: String) -> &u64 {
         return self
             .var_store
             .get(&id)
-            .ok_or(InterpError::VariableNotFound(id));
+            .expect(format!("cannot find variable by id - '{}'", id).as_str());
     }
 
-    pub fn stack_pop(&mut self) -> Result<u64, InterpError> {
-        return self.stack.pop().ok_or(InterpError::EmptyStack);
+    pub fn stack_pop(&mut self) -> u64 {
+        return self.stack.pop().expect("cannot pop from empty stack");
     }
 
     pub fn stack_push(&mut self, val: u64) {
         self.stack.push(val);
     }
 
-    pub fn from_str(&self, input: &str) -> Result<AstNode, InterpError> {
+    pub fn from_str(&self, input: &str) -> Result<AstNode, String> {
         let lexer_def = calc_l::lexerdef();
         let lexer = lexer_def.lexer(input);
         let (ast_exp, errs) = calc_y::parse(&lexer);
 
         let err_msg = self.get_parse_err(&lexer, errs);
         if err_msg.is_empty() == false {
-            return Err(InterpError::ParseError(err_msg));
+            return Err(err_msg);
         }
 
         match ast_exp {
             Some(res) => match res {
                 Ok(exp) => Ok(exp),
-                Err(_) => Err(InterpError::ParseError(err_msg)),
+                Err(_) => Err(err_msg),
             },
-            None => Err(InterpError::ParseError(err_msg)),
+            None => Err(err_msg),
         }
     }
 
@@ -96,33 +94,34 @@ impl Calc {
             AstNode::ID { value } => prog.push(Instruction::Load { id: value }),
         }
     }
-    pub fn eval(&mut self, instructions: &Vec<Instruction>) -> Result<Option<u64>, InterpError> {
+
+    pub fn eval(&mut self, instructions: &Vec<Instruction>) -> Result<Option<u64>, String> {
         for instruction in instructions {
             match instruction {
                 Instruction::Push { value } => self.stack.push(*value),
                 Instruction::PrintLn {} => {
-                    println!("{}", self.stack.pop().unwrap())
+                    println!("{}", self.stack.pop().expect("cannot pop from empty stack"))
                 }
                 Instruction::Mul {} => {
                     let val = self
-                        .stack_pop()?
-                        .checked_mul(self.stack_pop()?)
-                        .ok_or(InterpError::Numeric("overflowed".to_string()))?;
+                        .stack_pop()
+                        .checked_mul(self.stack.pop().expect("cannot pop from empty stack"))
+                        .ok_or("overflowed".to_string())?;
                     self.stack_push(val);
                 }
                 Instruction::Add {} => {
                     let val = self
-                        .stack_pop()?
-                        .checked_add(self.stack_pop()?)
-                        .ok_or(InterpError::Numeric("overflowed".to_string()))?;
+                        .stack_pop()
+                        .checked_add(self.stack.pop().expect("cannot pop from empty stack"))
+                        .ok_or("overflowed".to_string())?;
                     self.stack_push(val);
                 }
                 Instruction::Assign { id } => {
-                    let val = self.stack_pop()?;
+                    let val = self.stack_pop();
                     self.var_store.insert(id.to_string(), val);
                 }
                 Instruction::Load { id } => {
-                    self.stack_push(*self.get_var(id.into())?);
+                    self.stack_push(*self.get_var(id.into()));
                 }
             }
         }
